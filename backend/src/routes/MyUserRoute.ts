a7acf442a719976c1bf35757f5167b91340194b2
cementix <bodakondrackij@gmail.com>
2024-08-18 08:26:53 +0300

    backend user update validation

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 // Create router instance
 const router = express.Router();
@@ -9,6 +10,12 @@ const router = express.Router();
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 
 // PUT /api/my/user -> Update the current user
-router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;
