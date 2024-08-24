f8dc2163a7882515d9e759b655915fc8791476de
cementix <bodakondrackij@gmail.com>
2024-08-24 15:59:35 +0300

    backend endpoint for getting current user restaurant

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -3,6 +3,20 @@ import { Request, Response } from "express";
 import mongoose from "mongoose";
 import { Restaurant } from "../models/restaurant";
 
+const getMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant is not found" });
+    }
+
+    res.json(restaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error fetching restaurant" });
+  }
+};
+
 const createMyRestaurant = async (req: Request, res: Response) => {
   try {
     const existingRestaurant = await Restaurant.findOne({ user: req.userId });
@@ -33,4 +47,4 @@ const createMyRestaurant = async (req: Request, res: Response) => {
   }
 };
 
-export default { createMyRestaurant };
+export default { createMyRestaurant, getMyRestaurant };
